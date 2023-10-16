a2d6d53ac5fb132d36c8c93e984f956368eea559
olvrronelio <111504012+olvrronelio@users.noreply.github.com>
2023-10-16 07:52:38 -0300

    Teste Cancelamento Android

--- src/android/BrowserTab.java
+++ src/android/BrowserTab.java
@@ -12,145 +12,182 @@
  * limitations under the License.
  */
 
-package com.google.cordova.plugin.browsertab;
+ package com.google.cordova.plugin.browsertab;
 
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.net.Uri;
-import android.support.customtabs.CustomTabsIntent;
-import android.support.customtabs.CustomTabsClient;
-import android.util.Log;
-
-import java.util.Iterator;
-import java.util.List;
-
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaInterface;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.PluginResult;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.util.Collections;
-
-/**
- * Cordova plugin which provides the ability to launch a URL in an
- * in-app browser tab. On Android, this means using the custom tabs support
- * library, if a supporting browser (e.g. Chrome) is available on the device.
- */
-public class BrowserTab extends CordovaPlugin {
-
-  public static final int RC_OPEN_URL = 101;
-
-  private static final String LOG_TAG = "BrowserTab";
-
-  /**
-   * The service we expect to find on a web browser that indicates it supports custom tabs.
-   */
-  private static final String ACTION_CUSTOM_TABS_CONNECTION =
-          "android.support.customtabs.action.CustomTabsService";
-
-  private boolean mFindCalled = false;
-  private String mCustomTabsBrowser;
-
-  @Override
-  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
-    Log.d(LOG_TAG, "executing " + action);
-    if ("isAvailable".equals(action)) {
-      isAvailable(callbackContext);
-    } else if ("openUrl".equals(action)) {
-      openUrl(args, callbackContext);
-    }else if ("isCustomTabsSupported".equals(action)) {
-            isCustomTabsSupported(callbackContext);
-    } else if ("close".equals(action)) {
-      // close is a NOP on Android
-      return true;
-    } else {
-      return false;
-    }
-
-    return true;
-  }
-
-  private void isAvailable(CallbackContext callbackContext) {
-    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, true));
-  }
-
-  private void isCustomTabsSupported(CallbackContext callbackContext) {
-        String packageName = CustomTabsClient.getPackageName(
-                cordova.getActivity(),
-                Collections.emptyList()
-        );
-
-        System.out.println("################");
-        System.out.println(packageName);
-    
-        boolean customTabsSupported = (packageName != null);
-        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, customTabsSupported));
-    }
-
-  private void openUrl(JSONArray args, CallbackContext callbackContext) {
-    if (args.length() < 1) {
-      Log.d(LOG_TAG, "openUrl: no url argument received");
-      callbackContext.error("URL argument missing");
-      return;
-    }
-
-    String urlStr;
-    try {
-      urlStr = args.getString(0);
-    } catch (JSONException e) {
-      Log.d(LOG_TAG, "openUrl: failed to parse url argument");
-      callbackContext.error("URL argument is not a string");
-      return;
-    }
-
-    Intent customTabsIntent = new CustomTabsIntent.Builder().build().intent;
-    customTabsIntent.setData(Uri.parse(urlStr));
-    cordova.getActivity().startActivity(customTabsIntent);
-
-    Log.d(LOG_TAG, "in app browser call dispatched");
-    callbackContext.success();
-  }
-
-  private boolean isFullBrowser(ResolveInfo resolveInfo) {
-    // The filter must match ACTION_VIEW, CATEGORY_BROWSEABLE, and at least one scheme,
-    if (!resolveInfo.filter.hasAction(Intent.ACTION_VIEW)
-            || !resolveInfo.filter.hasCategory(Intent.CATEGORY_BROWSABLE)
-            || resolveInfo.filter.schemesIterator() == null) {
-        return false;
-    }
-
-    // The filter must not be restricted to any particular set of authorities
-    if (resolveInfo.filter.authoritiesIterator() != null) {
-        return false;
-    }
-
-    // The filter must support both HTTP and HTTPS.
-    boolean supportsHttp = false;
-    boolean supportsHttps = false;
-    Iterator<String> schemeIter = resolveInfo.filter.schemesIterator();
-    while (schemeIter.hasNext()) {
-        String scheme = schemeIter.next();
-        supportsHttp |= "http".equals(scheme);
-        supportsHttps |= "https".equals(scheme);
-
-        if (supportsHttp && supportsHttps) {
-            return true;
-        }
-    }
-
-    // at least one of HTTP or HTTPS is not supported
-    return false;
-  }
-
-  private boolean hasCustomTabWarmupService(PackageManager pm, String packageName) {
-    Intent serviceIntent = new Intent();
-    serviceIntent.setAction(ACTION_CUSTOM_TABS_CONNECTION);
-    serviceIntent.setPackage(packageName);
-    return (pm.resolveService(serviceIntent, 0) != null);
-  }
-}
+ import android.app.Activity;
+ import android.app.role.RoleManager;
+ import android.content.ActivityNotFoundException;
+ import android.content.Intent;
+ import android.content.pm.PackageManager;
+ import android.content.pm.ResolveInfo;
+ import android.net.Uri;
+ import android.os.Build;
+ import android.os.Bundle;
+ import android.provider.Settings;
+ import android.util.Log;
+ import android.webkit.WebChromeClient;
+ 
+ import androidx.browser.customtabs.CustomTabsClient;
+ import androidx.browser.customtabs.CustomTabsIntent;
+ 
+ import org.apache.cordova.CallbackContext;
+ import org.apache.cordova.CordovaPlugin;
+ import org.apache.cordova.LOG;
+ import org.apache.cordova.PluginResult;
+ import org.json.JSONArray;
+ import org.json.JSONException;
+ 
+ import java.util.Collections;
+ import java.util.Iterator;
+ 
+ /**
+  * Cordova plugin which provides the ability to launch a URL in an
+  * in-app browser tab. On Android, this means using the custom tabs support
+  * library, if a supporting browser (e.g. Chrome) is available on the device.
+  */
+ public class BrowserTab extends CordovaPlugin {
+ 
+   public static final int RC_OPEN_URL = 101;
+ 
+   private static final String LOG_TAG = "BrowserTab";
+ 
+   /**
+    * The service we expect to find on a web browser that indicates it supports custom tabs.
+    */
+   private static final String ACTION_CUSTOM_TABS_CONNECTION =
+           "android.support.customtabs.action.CustomTabsService";
+ 
+   private static final String SETTINGS_SELECT_OPTION_KEY = ":settings:fragment_args_key";
+   private static final String SETTINGS_SHOW_FRAGMENT_ARGS = ":settings:show_fragment_args";
+   private static final String DEFAULT_BROWSER_APP_OPTION = "default_browser";
+ 
+   @Override
+   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+     Log.d(LOG_TAG, "executing " + action);
+     if ("isAvailable".equals(action)) {
+       isAvailable(callbackContext);
+     } else if ("openUrl".equals(action)) {
+       openUrl(args, callbackContext);
+     }else // close is a NOP on Android
+       if ("isCustomTabsSupported".equals(action)) {
+             isCustomTabsSupported(callbackContext);
+     } else {
+         return "close".equals(action);
+     }
+ 
+     return true;
+   }
+ 
+   private void isAvailable(CallbackContext callbackContext) {
+     callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, true));
+   }
+ 
+   private void isCustomTabsSupported(CallbackContext callbackContext) {
+     String packages = CustomTabsClient.getPackageName(
+             cordova.getActivity(),
+             Collections.emptyList()
+     );
+ 
+     Intent intent = new Intent(Settings.ACTION_MANAGE_DEFAULT_APPS_SETTINGS);
+     intent.putExtra(
+             SETTINGS_SELECT_OPTION_KEY,
+             DEFAULT_BROWSER_APP_OPTION
+     );
+     Bundle bundle = new Bundle();
+     bundle.putString(SETTINGS_SELECT_OPTION_KEY, DEFAULT_BROWSER_APP_OPTION);
+     intent.putExtra(
+             SETTINGS_SHOW_FRAGMENT_ARGS,
+             bundle
+     );
+     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+     cordova.getActivity().startActivity(intent);
+ 
+     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+    /*   RoleManager roleManager = (RoleManager) getSystemService(ROLE_SERVICE);
+       Intent intent = roleManager.createRequestRoleIntent(RoleManager.ROLE_BROWSER);
+       resultLauncher.launch(intent);*/
+     } else {
+ 
+     }
+ 
+ 
+     boolean customTabsSupported = (packages != null);
+     callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, customTabsSupported));
+   }
+ 
+   private void openUrl(JSONArray args, CallbackContext callbackContext) {
+     if (args.length() < 1) {
+       Log.d(LOG_TAG, "openUrl: no url argument received");
+       callbackContext.error("URL argument missing");
+       return;
+     }
+ 
+     String urlStr;
+     try {
+       urlStr = args.getString(0);
+     } catch (JSONException e) {
+       Log.d(LOG_TAG, "openUrl: failed to parse url argument");
+       callbackContext.error("URL argument is not a string");
+       return;
+     }
+ 
+     Intent customTabsIntent = new CustomTabsIntent.Builder().build().intent;
+     customTabsIntent.setData(Uri.parse(urlStr));
+     //cordova.getActivity().startActivity(customTabsIntent);
+ 
+     try {
+       cordova.startActivityForResult(new CordovaPlugin() {
+         @Override
+         public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+           if (resultCode !=  Activity.RESULT_OK) {
+             callbackContext.error("ERROR_CANCELED_BY_USER");
+           } else {
+             callbackContext.success();
+           }
+         }
+       }, customTabsIntent, -20231016);
+     } catch (ActivityNotFoundException e) {
+       LOG.w("No activity found to handle file chooser intent.", e);
+       callbackContext.error("No activity found to handle the intent.");
+     }
+   }
+ 
+   private boolean isFullBrowser(ResolveInfo resolveInfo) {
+     // The filter must match ACTION_VIEW, CATEGORY_BROWSEABLE, and at least one scheme,
+     if (!resolveInfo.filter.hasAction(Intent.ACTION_VIEW)
+             || !resolveInfo.filter.hasCategory(Intent.CATEGORY_BROWSABLE)
+             || resolveInfo.filter.schemesIterator() == null) {
+         return false;
+     }
+ 
+     // The filter must not be restricted to any particular set of authorities
+     if (resolveInfo.filter.authoritiesIterator() != null) {
+         return false;
+     }
+ 
+     // The filter must support both HTTP and HTTPS.
+     boolean supportsHttp = false;
+     boolean supportsHttps = false;
+     Iterator<String> schemeIter = resolveInfo.filter.schemesIterator();
+     while (schemeIter.hasNext()) {
+         String scheme = schemeIter.next();
+         supportsHttp |= "http".equals(scheme);
+         supportsHttps |= "https".equals(scheme);
+ 
+         if (supportsHttp && supportsHttps) {
+             return true;
+         }
+     }
+ 
+     // at least one of HTTP or HTTPS is not supported
+     return false;
+   }
+ 
+   private boolean hasCustomTabWarmupService(PackageManager pm, String packageName) {
+     Intent serviceIntent = new Intent();
+     serviceIntent.setAction(ACTION_CUSTOM_TABS_CONNECTION);
+     serviceIntent.setPackage(packageName);
+     return (pm.resolveService(serviceIntent, 0) != null);
+   }
+ }
+ 
